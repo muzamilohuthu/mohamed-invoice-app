a9a7b06b35152a4470621a08ba1a74094caf1b02
muzamilohuthu <133840218+muzamilohuthu@users.noreply.github.com>
2023-09-01 22:06:59 +0530

    1.9

--- my-invoice/src/App.js
+++ my-invoice/src/App.js
@@ -1,11 +1,18 @@
+
 import './App.css';
 import Page1 from './pages/Page1';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Container from 'react-bootstrap/Container';
+
+
 function App() {
   return (
-    <div className="App d-flex flex-column align-item-center justify-content-center w-80 ">
-     <Page1 /> </div>
+    <div className="App d-flex flex-column align-item-center justify-content-center w-100">
+       <Container>
+       <Page1 /> 
+      </Container>
+      
     </div>
   );
 }
--- my-invoice/src/pages/Page1.jsx
+++ my-invoice/src/pages/Page1.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Form, Card ,Row ,Col }from 'react-bootstrap';
 
 function Page1() {
+  const [state ,setState] = useState({
+    isopen: false,
+    currency: '$',
+    currentDate: '',
+    invoiceNumber:1,
+    billTo:'',
+    billToAddress:'',
+    billToEmail:'',
+    billFrom:'MUZAMIL STORE',
+    billFromEmail:'muzamilohuthu@gmail.com',
+    billFromAdress:'Coimbatore,Tamilnadu,India',
+    notes:'',
+    subTotal:'0.00',
+    taxRate: 0.00,
+    taxAmount:'0.00',
+    discountRate:0.00,
+    discountAmount:'0.00'
+  })
+
+  const[total ,setTotal]= useState(0.00)
+
+  const[items,setItems]=useState(
+    {
+    id:0,
+    name:'',
+    description:'',
+    price:1.0,
+    quantity: 1,
+  }
+)
+
   return (
     <div>
-      <h2 className='w-50'>Invoice Generator</h2>
+      <h2>Invoice Generator</h2>
+
+      <Form>
+      <Row>
+        <Col md={8} lg={9}>
+        <Card className='p-4 p-xl- my-3 my-xl-4 d-flex flex-row justify-content-between'>
+        <div className='d-flex flex-row mb-3 '>
+         <div className='mb-2'>
+          <span className='fw-bold'> current Date: </span> 
+          <span className='currentDate'> 
+          {new Date().toLocaleDateString()}
+          </span>
+         </div>
+        </div>
+
+        <div className='d-flex flex-row  mb-3 '>
+         <div className='mb-2'>
+          <span className='fw-bold'> Invoice Number: </span> 
+          <span className='currentDate'> 
+          {state.invoiceNumber}
+          </span>
+         </div>
+        </div>
+
+
+
+        </Card>
+
+
+
+        </Col>
+      </Row>
+      </Form>
+
     </div>
   )
 }
